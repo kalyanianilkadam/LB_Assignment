47f741712d7709c19c5963216ca8fbcde87f4816
kalyanianilkadam <kalyanianilkadam@gmail.com>
2026-01-10 14:25:11 +0530

    Assignment27 Solution(Q1-Q5)

---
+++ Assignment no.27/Question3.c
@@ -0,0 +1,54 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   It is used to display Pattern
+//  Input :         int 
+//  Output :        int 
+//  Author :        Kalyani Anil Kadam.
+//  Date :          2/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j =0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1 ; j--)
+        {
+            printf("%d\t", j);
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function For the Application 
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+     
+    printf("Enter number of rows:");
+    scanf("%d", &iValue1);
+
+     printf("Enter number of columns:");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+}
