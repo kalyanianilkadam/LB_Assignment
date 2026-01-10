e52bfb723b4f7cf76d46a072f52e463bb2cc02e9
kalyanianilkadam <kalyanianilkadam@gmail.com>
2026-01-10 14:27:02 +0530

    Assignment29 Solution(Q1-Q5)

---
+++ Assignment no.29/Question1.c
@@ -0,0 +1,57 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   It is used to display Pattern
+//  Input :         int 
+//  Output :        int 
+//  Author :        Kalyani Anil Kadam.
+//  Date :          2/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j =0;
+    int iNo = 1;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol ; j++)
+        {
+            printf("%d\t", iNo);
+            iNo++;
+            if(iNo>9)
+            iNo = 1;
+        }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function For the Application 
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+     
+    printf("Enter number of rows:");
+    scanf("%d", &iValue1);
+
+     printf("Enter number of columns:");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+}
---
+++ Assignment no.29/Question4.c
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  Required Header Files 
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Pattern
+//  Description :   It is used to display Pattern
+//  Input :         int 
+//  Output :        int 
+//  Author :        Kalyani Anil Kadam.
+//  Date :          2/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i % 2 != 0)          
+            {
+                printf("%d\t", j);
+            }
+            else                    
+            {
+                printf("%d\t", -j);
+            }
+        }
+        printf("\n");
+    }
+}
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function For the Application 
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+     
+    printf("Enter number of rows:");
+    scanf("%d", &iValue1);
+
+     printf("Enter number of columns:");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+}
